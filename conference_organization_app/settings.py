2479fb4dde0d176244e79b9083d6b15354cecf53
Petros Kalogiannakis <kalpetros@gmail.com>
2016-01-13 16:53:50 +0200

    Initial working code

---
+++ conference_organization_app/settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '1084211039112-842peoh91l4jg8c0m802o4im88g2liev.apps.googleusercontent.com'
+
